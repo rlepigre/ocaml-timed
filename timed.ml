d56bc03af733a09d0d741507489b9c92148ec2c5
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-07-02 23:18:17 +0200

    Optimization by removing option.

--- timed.ml
+++ timed.ml
@@ -1,32 +1,32 @@
 module Time =
   struct
-    type t = T : {mutable p : t option; r : 'a ref; mutable v : 'a} -> t
+    type t = T : {mutable p : t; r : 'a ref; mutable v : 'a} -> t
 
     let current : t ref =
-      ref (T {p = None; r = ref 0; v = 0})
+      ref (let rec p = T {p; r = ref 0; v = 0} in p)
 
-    let save : unit -> t = fun () -> !current
+    let save : unit -> t =
+      fun () -> !current
 
     let restore : t -> unit =
-      let rec fn acc time =
-        match time with
-        | T {p = Some t} -> fn (time::acc) t
-        | _              -> gn acc time
-      and gn acc (T r as t0) =
+      let rec gn acc (T r as t0) =
         let v = r.v in
-        r.v <- !(r.r);
-        r.r := v;
+        r.v <- !(r.r); r.r := v;
         match acc with
-        | []     -> r.p <- None; current := t0
-        | t::acc -> r.p <- Some t; gn acc t
+        | []     -> r.p <- t0; current := t0
+        | t::acc -> r.p <- t; gn acc t
+      in
+      let rec fn acc ((T {p}) as time) =
+        if time != p then fn (time::acc) p else gn acc time
       in
       fn []
   end
 
 let (:=) : 'a ref -> 'a -> unit = fun r v ->
   let open Time in
-  let node = T {p = None; r; v = !r} in
-  match !current with T n -> n.p <- Some node; current := node; r := v
+  let rec node = T {p = node; r; v = !r} in
+  let T curr = !current in
+  curr.p <- node; current := node; r := v
 
 let incr : int ref -> unit = fun r -> r := !r + 1
 let decr : int ref -> unit = fun r -> r := !r - 1
