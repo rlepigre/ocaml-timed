4200ba008266a0b682533980d4285f4e3cc98999
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-07-03 01:12:46 +0200

    Working.

--- timed.ml
+++ timed.ml
@@ -1,30 +1,35 @@
 module Time =
   struct
-    type t = T : {mutable p : t; r : 'a ref; mutable v : 'a} -> t
+    type t = T : {mutable p : t; mutable f : bool; r : 'a ref; mutable v : 'a} -> t
 
     let current : t ref =
-      ref (let rec p = T {p; r = ref 0; v = 0} in p)
+      ref (let rec p = T {p; f = true; r = ref 0; v = 0} in p)
 
     let save : unit -> t =
       fun () -> !current
 
     let restore : t -> unit =
-      let rec gn acc (T r as t0) =
-        let v = r.v in
-        r.v <- !(r.r); r.r := v;
+      let rec gn f acc (T r as t0) =
+        let b =
+          match acc with
+          | []       -> true
+          | T {f}::_ -> not f
+        in
+        if not (f && b) then
+          (let v = r.v in r.v <- !(r.r); r.r := v);
         match acc with
-        | []     -> r.p <- t0; current := t0
-        | t::acc -> r.p <- t; gn acc t
+        | []     -> r.p <- t0; r.f <- true; current := t0
+        | t::acc -> r.p <- t; r.f <- not b; gn b acc t
       in
       let rec fn acc ((T {p}) as time) =
-        if time != p then fn (time::acc) p else gn acc time
+        if time != p then fn (time::acc) p else gn true acc time
       in
       fn []
   end
 
 let (:=) : 'a ref -> 'a -> unit = fun r v ->
   let open Time in
-  let rec node = T {p = node; r; v = !r} in
+  let rec node = T {p = node; f = true; r; v = !r} in
   let T curr = !current in
   curr.p <- node; current := node; r := v
 
