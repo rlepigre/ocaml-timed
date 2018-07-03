0fa810b803add88d1dfd4ebc920420b2907dc5c5
craff <raffalli@univ-savoie.fr>
2018-07-03 03:50:57 +0200

    complexity given in mli

--- timed.mli
+++ timed.mli
@@ -11,24 +11,38 @@ module Time :
     type t
 
     (** [save ()] registers the position of the program in its “timeline”. The
-        returned value can then be used to “time-travel” toward this point. *)
+        returned value can then be used to “time-travel” toward this point.
+        in O(1) *)
     val save : unit -> t
 
     (** [restore t] has the effect of “traveling in time” towards a previously
         recorded point [t]. After calling this function, (monitored) reference
-        updates made between [t] and the “time before the call” are undone. *)
+        updates made between [t] and the “time before the call” are undone.
+
+     Complexity: O(MT) when
+     - M: maximum number of references updated between two consecutive
+       save (the number of references, not the number of updates, hence,
+       is one update the same reference several time, it does not count)
+     - T: number of saved time between t and the current time.
+
+      A better complexity is the sum of all M_i for i in 0 to n-1 where t_i are the
+      time between t = t_0 and the current time t_n and M_i are the number of
+      updated references between t_i and t_(i+1).
+     *)
     val restore : t -> unit
   end
 
 type 'a ref
 
+(** reference access in O(1) *)
 val (!) : 'a ref -> 'a
 
+(** reference construction in O(1) *)
 val ref : 'a -> 'a ref
 
 (** [r := v] has the same effect as [Pervasives.(r := v)],  but the value that
     was stored in [r] before the update is recorded so that it may be restored
-    by a subsequent call to [Time.restore]. *)
+    by a subsequent call to [Time.restore]. in O(1) *)
 val (:=) : 'a ref -> 'a -> unit
 
 (** [incr r] increments the integer stored in [r],  recording the old value so
