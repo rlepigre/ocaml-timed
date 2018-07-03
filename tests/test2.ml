a11ed47102f40632303a1ff16eb472a611d7a0f1
craff <raffalli@univ-savoie.fr>
2018-07-03 11:37:40 +0200

    added command line options for test2

--- tests/test2.ml
+++ tests/test2.ml
@@ -1,33 +1,56 @@
-open Timed
+let steps = ref 10_000
+let nb_refs = ref 5
+let maxi = ref 5
+let proba_save = ref 0.2
+let proba_restore = ref 0.2
+let verbose = ref false
+let spec =
+  let open Arg in
+  [ ("--steps", Set_int steps, "number of steps in test")
+  ; ("--nb-refs", Set_int nb_refs, "number of references in the state")
+  ; ("--maxi", Set_int maxi, "maximum value of a referecence")
+  ; ("--proba-save", Set_float proba_save, "probability of a save at each step")
+  ; ("--proba-restore", Set_float proba_restore, "probability of a restore at each step")
+  ; ("--vebose", Set verbose, "verbose tests")
+  ]
 
-let nb_steps = 10_000
-let nb_ref = 5
-let maxi = 5
-let proba_save = 0.2
-let proba_restore = 0.2
+let _ = Arg.(parse spec (fun _ -> raise (Bad "useless arguments"))
+                   (Printf.sprintf "usage: %s [options]" Sys.argv.(0)))
+
+let steps = !steps
+let nb_refs = !nb_refs
+let maxi = !maxi
+let proba_save = !proba_save
+let proba_restore = !proba_restore
+let verbose = !verbose
+
+open Timed
 
 type fstate = int array
 type istate = int ref array
 type state = fstate * istate
 
 let init_state =
-  let f = Array.init nb_ref (fun _ -> Random.int maxi) in
+  let f = Array.init nb_refs (fun _ -> Random.int maxi) in
   let i = Array.map ref f in
   (f, i)
 
 let print_state (f, i) =
-  Array.iteri (fun j x ->
-      Printf.printf "%i: (%i, %i) ; " j x !(i.(j))) f;
-  print_newline ()
+  if verbose then
+    begin
+      Array.iteri (fun j x ->
+          Printf.printf "%i: (%i, %i) ; " j x !(i.(j))) f;
+      print_newline ()
+    end
 
 let compare_state (f,i) =
   Array.iteri (fun j x -> if !(i.(j)) <> x then assert false) f
 
 let update (f,i) =
-  let n = Random.int nb_ref in
+  let n = Random.int nb_refs in
   let x = Random.int maxi in
   let f = Array.copy f in
-    Printf.printf "update %i\n%!" n;
+  if verbose then Printf.printf "update %i\n%!" n;
   f.(n) <- x;
   i.(n) := x;
   (f,i)
@@ -36,13 +59,13 @@ let step saved (f,i as state) =
   let x = Random.float 1.0 in
   if x < 0.2 then
     let saved = saved @ [(f, Time.save ())] in
-    Printf.printf "save %i\n%!" (List.length saved - 1);
+    (if verbose then Printf.printf "save %i\n%!" (List.length saved - 1));
     (saved, state)
   else if x < 0.4 then
     let n = Random.int (List.length saved) in
     let (f, t) = List.nth saved n in
     Time.restore t;
-    Printf.printf "restore %i\n%!" n;
+    (if verbose then Printf.printf "restore %i\n%!" n);
     (saved, (f, i))
   else
     let state = update state in
@@ -51,7 +74,7 @@ let step saved (f,i as state) =
 let run () =
   let (f,_) as state = init_state in
   print_state state;
-  Printf.printf "save %i\n%!" 0;
+  if verbose then Printf.printf "save %i\n%!" 0;
   let saved = [(f, Time.save ())] in
   let rec fn count saved state =
     if count > 0 then
@@ -62,6 +85,6 @@ let run () =
     else
       ()
   in
-  fn nb_steps saved state
+  fn steps saved state
 
 let _ = run ()
