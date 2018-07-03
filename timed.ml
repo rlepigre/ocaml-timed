928db05fef5a29df9697262f75f1e1774f2b11c2
craff <raffalli@univ-savoie.fr>
2018-07-03 23:07:37 +0200

    commenting & cleaing Timed.Time implementation

--- timed.ml
+++ timed.ml
@@ -1,72 +1,103 @@
 module Time =
   struct
+    (* a type similar to ervasives.ref. The field 'w' stores that time (an
+        int identifyinh the time returned by save) of the last update. The
+        idea is to store only the value before the first update between two
+        consecutive time *)
     type 'a tref = { mutable contents : 'a; mutable w : int }
-    and memo = M : { r : 'a tref; mutable v : 'a } -> memo
-    and edge = {mutable d : node; mutable u : memo list }
-    and  node = edge
 
+    (* memo is a GADT to store the previous value of a reference *)
+    type memo = M : { r : 'a tref; mutable v : 'a } -> memo
+
+    (* This is then main structure of or librarie, it is an oriented graph,
+        whose nodes represent the time returned by save. Each node point to
+        a destination node [d], with u storing all updates that should be undone when
+       when goig to the time [d] *)
+    type node = {mutable d : node; mutable u : memo list }
+    type t = node
+
+    (* a counter to associate an integer to each node. We do not need to store
+        the integer in the node. *)
     let count = ref 0
 
+    (* Creation of a new node for the current time. The current time is characterised
+        by a loop that stores the latest update. *)
     let loop ()=
       incr count;
       let rec n = { d = n; u = [] } in n
 
-    let (!!) r = r.contents
+    (* the current time is a weak point, because if no saved time are
+       accessible, we do not need to keep a node, as this would keep useless
+       pointers *)
+    let current : node Weak.t = Weak.create 1
 
+    (* [reverse n] reverses the edge from [n] to [n.d] and undo all changes
+        in [n.u]. [n.u]  is updated to allow "redo" *)
     let reverse : node -> unit = fun s ->
       (* Reverse the pointers. *)
       let d = s.d in
-      List.iter (fun (M({r;v} as rc)) -> rc.v <- !!r; r.contents <- v;) s.u;
+      List.iter (fun (M({r;v} as rc)) -> rc.v <- r.contents; r.contents <- v;) s.u;
       d.d <- s; d.u <- s.u
 
-    type t = node
-
-    let current : node Weak.t = Weak.create 1
-
+    (* function returning the current time *)
     let save : unit -> t =
       fun () ->
       match Weak.get current 1 with
-      | None ->
+      | None -> (* We just create a new node *)
          let n = loop () in
          Weak.set current 1 (Some n);
          n
       | Some c ->
-        assert (c.d == c);
-        if c.u = [] then c else
-          let n = loop () in
-          c.d <- n;
-          Weak.set current 1 (Some n);
-          n
+         assert (c.d == c);
+         if c.u = [] then c else
+           (* if some undo are recorded, we need to create a node "after" these undo *)
+           let n = loop () in
+           c.d <- n;
+           Weak.set current 1 (Some n);
+           n
 
+    (* [restore t] restores the value of all pointer at time [t]. *)
     let restore : t -> unit = fun t ->
-      let rec gn acc t0 =
-        (* Undo the references. *)
-        match acc with
-        | []       ->  t0.d <- t0; t0. u <- []; Weak.set current 1 (Some t0); incr count;
-        | t::acc -> assert (t.d == t0); reverse t; gn acc t
-      in
+      (* fn builds the path from [t] to the current time and call gn *)
       let rec fn acc t =
         let d = t.d in
         if d == t then (reverse d; gn acc d)
         else fn (t::acc) d
-      in
-      fn [] t
+      (* gn really do the undo *)
+      and  gn acc t0 =
+        (* Undo the references. *)
+        match acc with
+        | []       ->  assert (t0 == t);
+                       (* t becomes the current time *)
+                       t0.d <- t0; t0. u <- []; Weak.set current 1 (Some t0); incr count;
+        | t::acc -> assert (t.d == t0);
+                    (* we reverse the edge from t to t0, which performs the undo *)
+                    reverse t; gn acc t
+      in fn [] t
 
+    (* referece creation *)
     let ref x = {
         contents = x;
         w = 0
       }
 
+    (* update *)
     let (:=) : 'a tref -> 'a -> unit = fun r v ->
       begin
+        (* no need to store the old value if
+            - the current time is not accessible (no saved time are accessible)
+            - or [r] was already updated and its initial value is already stored *)
         match Weak.get current 1, r.w <> !count with
         | (Some c, true) ->
            assert (c.d == c);
-           let m = M {r; v = !!r} in
+           (* we store the old value *)
+           let m = M {r; v = r.contents} in
            c.u <-m :: c.u;
+           (* we store the information that r was updated at the current time *)
            r.w <- !count
         | _ -> ()
       end;
+      (* and we do the update! *)
       r.contents <- v;
   end
 
@@ -74,7 +105,7 @@ type 'a ref = 'a Time.tref
 
 let ref = Time.ref
 
-let (!) =  Time.(!!)
+let (!) r =  Time.(r.contents)
 
 let (:=) = Time.(:=)
 
