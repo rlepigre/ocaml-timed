d29271944c691baaa0211d4a52fa1e4ce7136407
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-07-04 19:53:25 +0200

    More cleaning of [timed.ml].

--- timed.ml
+++ timed.ml
@@ -3,7 +3,7 @@ type 'a ref =
   { mutable contents : 'a
   ; mutable last_uid : int }
 
-(* NOTE [last_uid] stores the unique identifier of the [node] (see below) that
+(* NOTE [last_uid] stores the unique identifier of the “node” (see below) that
    contains the last update information for the reference.  It is used to only
    store the first update to a reference in between two “snapshots”. *)
 
@@ -13,15 +13,38 @@ let (!) : 'a ref -> 'a = fun r -> r.contents
 
 module Time =
   struct
-    (* memo is a GADT to store the previous value of a reference *)
-    type memo = M : { r : 'a ref; mutable v : 'a } -> memo
+    (* Type used to store the previous value of a reference. *)
+    type memo = M : {r : 'a ref; mutable v : 'a} -> memo
 
-    (* This is then main structure of or librarie, it is an oriented graph,
-        whose nodes represent the time returned by save. Each node point to
-        a destination node [d], with u storing all updates that should be undone when
-       when goig to the time [d] *)
-    type node = {mutable d : node; mutable u : memo list }
+    (* The main data structure is an oriented graph that is held by one of its
+       nodes, and stored on the OCaml heap. This means that parts of the graph
+       that are not accessible (in terms of pointers) can be collected, and we
+       can consider that they are not part of the graph. Every node contains a
+       destination node [d], and list [u] of updates that can be undone. *)
+    type node = {mutable d : node; mutable u : memo list}
     type t = node
+
+    (* NOTE We require the in-memory graph to be either empty, or to be a tree
+       oriented toward its root. Intuitively, the root (if any) will represent
+       the [current] time, and any other [node] in the tree represents a point
+       to which we can travel (following pointer backwards, from a given point
+       to the root of the tree). *)
+
+    (* Root of the in-memory tree, or current “time”. *)
+    let current : node Weak.t = Weak.create 1
+
+    (* NOTE The [current] “time” is implemented as a weak pointer so that  the
+       root of the tree can be collected (and the graph made empty) if none of
+       the other nodes are accessible (the “time” has not been saved). This is
+       useful to save memory in the cases where the state is never saved. *)
+
+    (* Get and set operation for the [current] root node. *)
+    let get_current : unit -> node option = fun _ -> Weak.get current 0
+    let set_current : node -> unit = fun n -> Weak.set current 0 (Some(n))
+
+    (* NOTE At a low level, restoring a previously saved “time” really amounts
+       to setting the corresponding node to be the root of the tree (reversing
+       the pointers, and undoing the updates in the process). *)
 
     (* a counter to associate an integer to each node. We do not need to store
         the integer in the node. *)
@@ -33,14 +56,6 @@ module Time =
       Pervasives.incr count;
       let rec n = { d = n; u = [] } in n
 
-    (* the current time is a weak point, because if no saved time are
-       accessible, we do not need to keep a node, as this would keep useless
-       pointers *)
-    let current : node Weak.t = Weak.create 1
-
-    let get_cur () = Weak.get current 0
-    let set_cur x = Weak.set current 0 x
-
     (* [reverse n] reverses the edge from [n] to [n.d] and undo all changes
         in [n.u]. [n.u]  is updated to allow "redo" *)
     let reverse : node -> unit = fun s ->
@@ -50,12 +65,11 @@ module Time =
       d.d <- s; d.u <- s.u
 
     (* function returning the current time *)
-    let save : unit -> t =
-      fun () ->
-      match get_cur () with
+    let save : unit -> t = fun () ->
+      match get_current () with
       | None -> (* We just create a new node *)
          let n = loop () in
-         set_cur (Some n);
+         set_current n;
          n
       | Some c ->
          assert (c.d == c);
@@ -63,7 +77,7 @@ module Time =
            (* if some undo are recorded, we need to create a node "after" these undo *)
            let n = loop () in
            c.d <- n;
-           set_cur (Some n);
+           set_current n;
            n
 
     (* [restore t] restores the value of all pointer at time [t]. *)
@@ -79,31 +93,27 @@ module Time =
         match acc with
         | []       ->  assert (t0 == t);
                        (* t becomes the current time *)
-                       t0.d <- t0; t0. u <- []; set_cur (Some t0);
+                       t0.d <- t0; t0. u <- []; set_current t0;
                        Pervasives.incr count;
         | t::acc -> assert (t.d == t0);
                     (* we reverse the edge from t to t0, which performs the undo *)
                     reverse t; gn acc t
       in fn [] t
 
-    (* update *)
+    (* Reference update. *)
     let (:=) : 'a ref -> 'a -> unit = fun r v ->
       begin
-        (* no need to store the old value if
-            - the current time is not accessible (no saved time are accessible)
-            - or [r] was already updated and its initial value is already stored *)
-        match get_cur (), r.last_uid <> Pervasives.(!count) with
-        | (Some c, true) ->
-           assert (c.d == c);
-           (* we store the old value *)
-           let m = M {r; v = r.contents} in
-           c.u <-m :: c.u;
-           (* we store the information that r was updated at the current time *)
-           r.last_uid <- Pervasives.(!count)
-        | _ -> ()
+        (* No need to store the previous value if it has already been updated
+           inside the same node. *)
+        if r.last_uid <> Pervasives.(!count) then
+        match get_current () with
+        | None    -> () (* Current time not accessible, no need to save. *)
+        | Some(c) ->
+            assert (c.d == c); (* Check that the root points to itself. *)
+            c.u <- M {r; v = r.contents} :: c.u; (* Save the old value. *)
+            r.last_uid <- Pervasives.(!count) (* Last set at current time. *)
       end;
-      (* and we do the update! *)
-      r.contents <- v;
+      r.contents <- v (* Actual update. *)
   end
 
 (* Reference update. *)
